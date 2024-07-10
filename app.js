510e94c9340b78769356e07d0a2da3d2e8a9ac05
ramrshrcg <107596066+ramrshrcg@users.noreply.github.com>
2024-07-10 14:27:28 +0545

    day 4

--- app.js
+++ app.js
@@ -1,61 +1,86 @@
-const express= require ("express")
-const connectToDb=require("./database/databaseConnection.js")
-const Blog = require("./model/blogModel.js") 
-const app = express()
+const express = require("express");
+const connectToDb = require("./database/databaseConnection.js");
+const Blog = require("./model/blogModel.js");
+const app = express();
 // require("./middleware/multerconfig.js").multer
-const {multer, storage}= require ('./middleware/multerconfig.js')
-const upload = multer({storage: storage})
+const { multer, storage } = require("./middleware/multerconfig.js");
+const upload = multer({ storage: storage });
 
-connectToDb()
-app.use(express.json())
-app.use(express.urlencoded({extended  : true}))
-app.use(express.static("./images"))
+connectToDb();
 
-app.set('view engine ','ejs')
-app.get("/",async (req, res)=>{
-    const blogs = await Blog.find()//alwYS REturns array 
-    
-    // if(blogs.length ==0)
-    // {
-    //     res.send('no blogs')
-    // }
-    console.log(blogs);
-    res.render("blogs/homepage.ejs",{blogs:blogs})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+
+app.set("view engine ", "ejs");
+app.use(express.static("./storage"));
+
+app.get("/", async (req, res) => {
+  const blogs = await Blog.find(); //alwYS REturns array
+
+  // console.log(blogs);
+  res.render("blogs/homepage.ejs", { blogs: blogs });
+});
+
+app.get("/about", (req, res) => {
+  const name = "ramesh";
+  res.render("about.ejs", { name });
+});
+
+app.get("/createblog", (req, res) => {
+  res.render("createblog.ejs");
+});   
+
+app.post("/createblog", upload.single("image"), async (req, res) => {
+  const filename = req.file.filename;
+  const { title, subtitle, description } = req.body;
+  console.log(title, subtitle, description);
+  await Blog.create({
+    title,
+    subtitle,
+    description,
+    image: filename,
+  });
+  res.send("bxlog maded  sucessfully");
+  res.redirect("/")
+});
+
+app.get("/blog/:id",async (req,res)=>{
+  const id = req.params.id
+  const blog = await Blog.findById(id)
+  res.render("./blogs/singlepage",{blog:blog})
 })
 
-app.get("/about",(req, res)=>{
-  const name="ramesh"
-    res.render("about.ejs",{name})
+app.get("/deleteblog/:id",async (req,res)=>{
+  const id = req.params.id 
+  await Blog.findByIdAndDelete(id)
+  res.redirect("/")
+})
+app.get("/editblog/:id",(req,res)=>{
+    res.render("./blogs/editBlog")
 })
 
-app.get("/blog", (req, res)=>{
-    res.render("createblog.ejs")
-})
+ /*/*/ /* *//////////////////////////** *////////////////
+app.get("/myblog", async (req, res) => {
+  const myblog = await Blog.find({});
 
- app.post("/blog",upload.single('image'),async (req, res)=> {
-    const filename= req.file.filename
-    const {title, subtitle, description }= req.body
-    console.log(title, subtitle, description)
-     await Blog.create({
-        title, 
-        subtitle,
-        description,
-        image:filename
-    })
-    res.send("bxlog sent sucessfully")
-})
-app.get("/home", async(req, res)=>{
-    const blogs = await Blog.find()//alwYS REturns array 
-    console.log(blogs)
-    res.render("blogs/homepage.ejs",{blogs:blogs})
-})
+  res.render("blogs/myblog.ejs", { myblog });
+});
+
+// app.get("/home", async (req, res) => {
+  // const blogs = await Blog.find();
+  // console.log(blogs);
+  // res.render("blogs/homepage.ejs", { blogs: blogs });
+// });
 
 
 
-app.listen(3000,()=>{
-    console.log("node has started"+3000)
-})
-app.get("/form", (req, res)=>{
-    res.render("form.ejs") 
-})
+
+// app.get("/form", (req, res) => {
+//   res.render("form.ejs");
+// });//deleted form page
+
+
+app.listen(3000, () => {
+  console.log("node has started" + 3000);
+});
